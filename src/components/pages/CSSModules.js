dcc37f6b49e1868fe7aa31a60d78782046bc87c6
Rudin Swagerman <post@rudinswagerman.nl>
2016-03-30 16:03:14 +0200

    CSSModules: now only links to styles object

--- src/components/pages/CSSModules.js
+++ src/components/pages/CSSModules.js
@@ -2,33 +2,33 @@ import React from 'react';
 import PageContents from 'PageContents/PageContents';
 import styles from './CSSModules.css';
 export default () => (
-  <div className={styles.main}>
+  <div className={`${styles.main} mdl-js-layout`}>
     <header className={styles.header}>
       <div className={styles.headerRow}>
         <span className={styles.headerTitle}>The Name Of This Page</span>
         <div className={styles.spacer}></div>
-        <div className={styles.textField}>
-          <label className={styles.searchButton} htmlFor="search">
+        <div className={`${styles.textField} mdl-js-textfield`}>
+          <label className={`${styles.searchButton} mdl-js-button`} htmlFor="search">
             <i className={styles.icon}>search</i>
           </label>
           <div className={styles.textfieldContainer}>
-            <input className="mdl-textfield__input" type="text" id="search"/>
-            <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
+            <input className={styles.input} type="text" id="search"/>
+            <label className={styles.label} htmlFor="search">Enter your query...</label>
           </div>
         </div>
       </div>
     </header>
-    <div className="demo-ribbon"></div>
-    <main className="demo-main mdl-layout__content">
-      <div className="demo-container mdl-grid">
-        <div className="mdl-cell mdl-cell--2-col mdl-cell--hide-tablet mdl-cell--hide-phone"></div>
-        <div className="demo-content mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--8-col">
+    <div className={styles.demoRibbon}></div>
+    <main className={styles.demoMain}>
+      <div className={styles.demoContainer}>
+        <div className={styles.cell}></div>
+        <div className={styles.demoContent}>
           <PageContents />
         </div>
       </div>
-      <footer className="demo-footer mdl-mini-footer">
-        <div className="mdl-mini-footer--left-section">
-          <ul className="mdl-mini-footer--link-list">
+      <footer className={styles.demoFooter}>
+        <div className={styles.demoFooterLeftSection}>
+          <ul className={styles.demoFooterLinkList}>
             <li><a href="#">Help</a></li>
             <li><a href="#">Privacy and Terms</a></li>
             <li><a href="#">User Agreement</a></li>
